b88736958fba0c5dca94d6535ef6affa4a1d2a6e
zhengjunpeng <wabbybabbo@outlook.com>
2025-01-08 20:13:04 +0800

    新增功能：通过邮件的方式自动提醒用户归还借阅逾期的书籍。

--- admin-service/src/main/java/org/example/admin/task/BorrowTask.java
+++ admin-service/src/main/java/org/example/admin/task/BorrowTask.java
@@ -1,5 +1,6 @@
 package org.example.admin.task;
 
+import cn.hutool.extra.mail.MailUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
@@ -7,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.admin.entity.Book;
 import org.example.admin.entity.Borrow;
+import org.example.admin.entity.User;
 import org.example.admin.mapper.BookMapper;
 import org.example.admin.mapper.BorrowMapper;
+import org.example.admin.mapper.UserMapper;
 import org.example.common.constant.BorrowStatusConstant;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -28,6 +31,7 @@ public class BorrowTask {
 
     private final BorrowMapper borrowMapper;
     private final BookMapper bookMapper;
+    private final UserMapper userMapper;
 
     /**
      * 定时处理状态为'借阅中'的借阅记录
@@ -36,20 +40,30 @@ public class BorrowTask {
     public void processBorrow() {
         log.info("[log] 定时处理状态为'借阅中'的借阅记录");
         // 查询当前日期超过预计归还日期的借阅记录
-        LambdaQueryWrapper<Borrow> queryWrapper = new LambdaQueryWrapper<Borrow>()
-                .select(Borrow::getId)
+        LambdaQueryWrapper<Borrow> queryWrapper1 = new LambdaQueryWrapper<Borrow>()
+                .select(Borrow::getId, Borrow::getUserId)
                 .eq(Borrow::getStatus, BorrowStatusConstant.BORROW)
                 .lt(Borrow::getReturnDate, LocalDate.now());
-        List<Borrow> borrows = borrowMapper.selectList(queryWrapper);
+        List<Borrow> borrowList = borrowMapper.selectList(queryWrapper1);
 
-        if(borrows.isEmpty()) return;
+        if (borrowList.isEmpty()) return;
 
         // 将这些借阅记录的借阅状态更改为'未按时归还'
-        List<String> borrowIds = borrows.stream().map(Borrow::getId).collect(Collectors.toList());
+        List<String> borrowIdList = borrowList.stream().map(Borrow::getId).collect(Collectors.toList());
         LambdaUpdateWrapper<Borrow> updateWrapper = new LambdaUpdateWrapper<Borrow>()
                 .set(Borrow::getStatus, BorrowStatusConstant.RETURN_OVERDUE)
-                .in(Borrow::getId, borrowIds);
+                .in(Borrow::getId, borrowIdList);
         borrowMapper.update(updateWrapper);
+
+        // 通过邮件的方式提醒用户归还借阅逾期的书籍
+        List<String> userIdList = borrowList.stream().map(Borrow::getUserId).collect(Collectors.toList());
+        LambdaQueryWrapper<User> queryWrapper2 = new LambdaQueryWrapper<User>()
+                .select(User::getEmail)
+                .in(User::getId, userIdList);
+        List<String> emailList = userMapper.selectList(queryWrapper2).stream().map(User::getEmail).toList();
+        for (String email : emailList) {
+            MailUtil.send(email, "【书店借阅平台】", "您有未归还的借阅书籍，请尽快归还。", true);
+        }
     }
 
     /**
@@ -66,7 +80,7 @@ public class BorrowTask {
                 .lt(Borrow::getReserveDate, LocalDate.now());
         List<Borrow> borrows = borrowMapper.selectList(queryWrapper);
 
-        if(borrows.isEmpty()) return;
+        if (borrows.isEmpty()) return;
 
         // 将这些借阅记录的借阅状态更改为'预约已失效'
         List<String> borrowIds = borrows.stream().map(Borrow::getId).collect(Collectors.toList());
@@ -75,11 +89,11 @@ public class BorrowTask {
                 .in(Borrow::getId, borrowIds);
         borrowMapper.update(updateWrapper1);
         // 并更改书籍库存 当前库存+1
-        List<String> isbns = borrows.stream().map(Borrow::getIsbn).collect(Collectors.toList());
+        List<String> isbnList = borrows.stream().map(Borrow::getIsbn).collect(Collectors.toList());
         LambdaUpdateWrapper<Book> updateWrapper2 = new UpdateWrapper<Book>()
                 .setSql("stock=stock+1")
                 .lambda()
-                .in(Book::getIsbn, isbns);
+                .in(Book::getIsbn, isbnList);
         bookMapper.update(updateWrapper2);
     }
 
